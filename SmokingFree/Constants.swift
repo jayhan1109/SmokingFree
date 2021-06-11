cc1219d7494ca3f1dedfdd9386c487b8b9f7e712
jayhan1109 <jungho1109@gmail.com>
2021-06-11 13:56:57 -0700

    Add constants

--- SmokingFree/Constants.swift
+++ SmokingFree/Constants.swift
@@ -14,10 +14,14 @@ struct K {
     
     static let isDoneKey = "isDone"
     
+    static let targetDay = "targetDay"
+    
     struct CurrentDate {
+        
         static let currentYear = NSCalendar.current.component(.year, from: Date())
         static let currentMonth = NSCalendar.current.component(.month, from: Date())
         static let currentWeek = NSCalendar.current.component(.weekOfYear, from: Date())
+        static let today = NSCalendar.current.component(.day, from: Date())
     }
     
 }
